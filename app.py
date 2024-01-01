4b55d759df02e647b231b94abbd7a9e876e20463
tiredwren <moudgalya.sneha@gmail.com>
2023-12-31 17:44:40 -0800

    change login

--- app.py
+++ app.py
@@ -49,9 +49,7 @@ def login():
 
     info, info1 = st.columns(2)
 
-    if not authentication_status:
-        st.write("or")
-        sign_up()
+    sign_up()
 
     if username:
         def current_user():
